7ff1064b3069418d304163907839233d58d3af57
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2024-03-24 22:54:14 +0000

    closing task on setting up a project

--- src/Api/Bitlink.User.Api/WeatherForecast.cs
+++ src/Api/Bitlink.User.Api/WeatherForecast.cs
@@ -5,7 +5,7 @@ public class WeatherForecast
     public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
-
+//will be deleted later
    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
